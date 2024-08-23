7944cfddc3fb07f322349e8f3ebdb09e7ec0ebf2
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2024-08-23 16:24:17 +0300

    add currency api services

--- composeApp/src/commonMain/kotlin/org/example/currency_converter/data/remote/api/CurrencyApiService.kt
+++ composeApp/src/commonMain/kotlin/org/example/currency_converter/data/remote/api/CurrencyApiService.kt
@@ -4,6 +4,5 @@ class CurrencyApiService {
     companion object {
         const val API_KEY = "cur_live_seGDRO7QhuQrv76Unsd847O580mWeDxl5xocQI40"
         const val ENDPOINT = "https://api.currencyapi.com/v3/latest"
-
     }
 }
