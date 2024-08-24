175e041079762332fb954a128a05b589b3c42bc2
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2024-08-24 14:17:21 +0300

    add plugins to dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.realm)
 }
 
 kotlin {
