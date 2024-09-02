0941551b6ed686e9298d2ebe83b9c324a9ded49c
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2024-09-02 22:39:11 +0300

    add http client in service and implement get exchange rates end point

--- composeApp/src/commonMain/kotlin/org/example/currency_converter/data/remote/api/CurrencyApiService.kt
+++ composeApp/src/commonMain/kotlin/org/example/currency_converter/data/remote/api/CurrencyApiService.kt
@@ -1,4 +1,8 @@
 package org.example.currency_converter.data.remote.api
 
+import org.example.currency_converter.data.remote.model.Currency
+import org.example.currency_converter.data.remote.model.RequestState
+
 interface CurrencyApiService {
+    suspend fun getExchangeRate(): RequestState<List<Currency>>
 }
--- composeApp/src/commonMain/kotlin/org/example/currency_converter/data/remote/api/CurrencyApiServiceImp.kt
+++ composeApp/src/commonMain/kotlin/org/example/currency_converter/data/remote/api/CurrencyApiServiceImp.kt
@@ -1,8 +1,49 @@
 package org.example.currency_converter.data.remote.api
 
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.example.currency_converter.data.remote.model.ApiResponse
+import org.example.currency_converter.data.remote.model.Currency
+import org.example.currency_converter.data.remote.model.RequestState
+
 class CurrencyApiServiceImp: CurrencyApiService {
     companion object {
         const val API_KEY = "cur_live_seGDRO7QhuQrv76Unsd847O580mWeDxl5xocQI40"
         const val ENDPOINT = "https://api.currencyapi.com/v3/latest"
+    }
+    private val httpClient = HttpClient{
+        install(ContentNegotiation){
+            json(Json{
+                ignoreUnknownKeys = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+        install(HttpTimeout){
+            requestTimeoutMillis = 15000
+        }
+        install(DefaultRequest){
+            headers.append("apikey", API_KEY)
+        }
+    }
+
+    override suspend fun getExchangeRate(): RequestState<List<Currency>> {
+        val response = httpClient.get(ENDPOINT)
+        return  try {
+            if (response.status.value == 200){
+                val apiResponse = Json.decodeFromString<ApiResponse>(response.body())
+                 RequestState.Success(apiResponse.data.values.toList())
+            }else {
+                 RequestState.Error("Error fetching data")
+            }
+        }catch (e: Exception){
+             RequestState.Error(e.message ?: "An error occurred")
+        }
     }
 }
